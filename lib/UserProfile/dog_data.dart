47fe36e0f62e350788ae355fc55c9e40e90c4983
rleszkiewicz <radekleszkiewicz@gmail.com>
2020-03-23 12:55:41 +0100

    profile view and dogs view

---
+++ lib/UserProfile/dog_data.dart
@@ -0,0 +1,9 @@
+class Dog {
+  String name;
+  String breed;
+  String color;
+  String description;
+  DateTime vaccinationDate;
+
+  Dog({this.name, this.breed, this.color, this.description, this.vaccinationDate});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:doggo_frontend/UserProfile/dogs_list.dart';
 import 'package:flutter/material.dart';
 import 'package:doggo_frontend/UserProfile/user_profile.dart';
 
 void main() => runApp(MaterialApp(
   initialRoute: '/userprofile',
   routes: {
-    '/userprofile': (context) => UserProfile()
+    '/userprofile': (context) => UserProfileView(),
+    '/dogsinfo': (context) => DogsList(),
   },
 ));
