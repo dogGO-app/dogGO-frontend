b953404dffb97828c19306e78e17cde870e58bcb
mnajborowski <michal.najborowski@gmail.com>
2020-11-14 17:43:13 +0100

    make oauth2_client fields private

--- lib/OAuth2/oauth2_client.dart
+++ lib/OAuth2/oauth2_client.dart
@@ -5,30 +5,30 @@ import 'package:oauth2/oauth2.dart' as oauth2;
 import 'package:path_provider/path_provider.dart';
 
 class OAuth2Client {
-  final authorizationEndpoint = Uri.parse(
+  final _authorizationEndpoint = Uri.parse(
       'https://doggo-auth-service.herokuapp.com/keycloak/realms/master/protocol/openid-connect/token');
 
-  final credentialsGrantIdentifier = 'doggo';
-  final passwordGrantIdentifier = 'doggo_services';
-  final credentialsGrantSecret = 'de8d431e-9bd7-4683-929f-2a55bb77f027';
-  final passwordGrantSecret = 'b11a8ae3-d625-457d-aeed-86f5ef2b4906';
+  final _credentialsGrantIdentifier = 'doggo';
+  final _passwordGrantIdentifier = 'doggo_services';
+  final _credentialsGrantSecret = 'de8d431e-9bd7-4683-929f-2a55bb77f027';
+  final _passwordGrantSecret = 'b11a8ae3-d625-457d-aeed-86f5ef2b4906';
 
-  File credentialsFile;
+  File _credentialsFile;
   
   Future<bool> _credentialFileExists() async {
     Directory directory = await getApplicationDocumentsDirectory();
-    credentialsFile = File('${directory.path}/credentials.json');
-    return await credentialsFile.exists();
+    _credentialsFile = File('${directory.path}/credentials.json');
+    return await _credentialsFile.exists();
   }
 
   Future<oauth2.Client> getClientCredentialsGrant() async {
     bool fileExists = await _credentialFileExists();
 
     var client = await oauth2.clientCredentialsGrant(
-        authorizationEndpoint, credentialsGrantIdentifier, credentialsGrantSecret);
+        _authorizationEndpoint, _credentialsGrantIdentifier, _credentialsGrantSecret);
 
     if (fileExists)
-      await credentialsFile.writeAsString(client.credentials.toJson());
+      await _credentialsFile.writeAsString(client.credentials.toJson());
     else
       throw Exception("Couldn't save credentials. File doesn't exist.");
 
@@ -40,11 +40,11 @@ class OAuth2Client {
     bool fileExists = await _credentialFileExists();
 
     var client = await oauth2.resourceOwnerPasswordGrant(
-        authorizationEndpoint, email, password,
-        identifier: passwordGrantIdentifier, secret: passwordGrantSecret);
+        _authorizationEndpoint, email, password,
+        identifier: _passwordGrantIdentifier, secret: _passwordGrantSecret);
 
     if (fileExists)
-      await credentialsFile.writeAsString(client.credentials.toJson());
+      await _credentialsFile.writeAsString(client.credentials.toJson());
     else
       throw Exception("Couldn't save credentials. File doesn't exist.");
 
@@ -53,20 +53,20 @@ class OAuth2Client {
 
   Future<oauth2.Client> loadCredentialsFromFile(BuildContext context) async {
     Directory directory = await getApplicationDocumentsDirectory();
-    credentialsFile = File('${directory.path}/credentials.json');
-    bool fileExists = await credentialsFile.exists();
+    _credentialsFile = File('${directory.path}/credentials.json');
+    bool fileExists = await _credentialsFile.exists();
 
     if (fileExists) {
       var credentials =
-      oauth2.Credentials.fromJson(await credentialsFile.readAsString());
+      oauth2.Credentials.fromJson(await _credentialsFile.readAsString());
       if (credentials.isExpired && credentials.canRefresh)
-        credentials = await credentials.refresh(identifier: passwordGrantIdentifier, secret: passwordGrantSecret);
+        credentials = await credentials.refresh(identifier: _passwordGrantIdentifier, secret: _passwordGrantSecret);
       else {
         DoggoToast.of(context).showToast("Credentials have expired.");
         Navigator.of(context).popUntil(ModalRoute.withName('/home'));
       }
       return oauth2.Client(credentials,
-          identifier: passwordGrantIdentifier, secret: passwordGrantSecret);
+          identifier: _passwordGrantIdentifier, secret: _passwordGrantSecret);
     }
     Navigator.of(context).popUntil(ModalRoute.withName('/home'));
     throw Exception("Credentials file doesn't exist.");
