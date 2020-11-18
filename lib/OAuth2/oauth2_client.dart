6359a8d23c45a13182470e5ec41ba5eeb74458d7
mnajborowski <michal.najborowski@gmail.com>
2020-11-18 15:57:54 +0100

    update oauth2_client

--- lib/OAuth2/oauth2_client.dart
+++ lib/OAuth2/oauth2_client.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:doggo_frontend/Custom/doggo_toast.dart';
 import 'package:flutter/material.dart';
-import 'package:oauth2/oauth2.dart' as oauth2;
+import 'package:oauth2/oauth2.dart';
 import 'package:path_provider/path_provider.dart';
 
 class OAuth2Client {
@@ -14,61 +14,49 @@ class OAuth2Client {
   final _passwordGrantSecret = 'b11a8ae3-d625-457d-aeed-86f5ef2b4906';
 
   File _credentialsFile;
-  
-  Future<bool> _credentialFileExists() async {
+
+  void _getCredentialsFile() async {
     Directory directory = await getApplicationDocumentsDirectory();
     _credentialsFile = File('${directory.path}/credentials.json');
-    return await _credentialsFile.exists();
+    if (!await _credentialsFile.exists())
+      _credentialsFile.create();
   }
 
-  Future<oauth2.Client> getClientCredentialsGrant() async {
-    bool fileExists = await _credentialFileExists();
+  Future<Client> getClientCredentialsGrant() async {
+    var client = await clientCredentialsGrant(_authorizationEndpoint,
+        _credentialsGrantIdentifier, _credentialsGrantSecret);
 
-    var client = await oauth2.clientCredentialsGrant(
-        _authorizationEndpoint, _credentialsGrantIdentifier, _credentialsGrantSecret);
-
-    if (fileExists)
-      await _credentialsFile.writeAsString(client.credentials.toJson());
-    else
-      throw Exception("Couldn't save credentials. File doesn't exist.");
+    await _getCredentialsFile();
+    await _credentialsFile.writeAsString(client.credentials.toJson());
 
     return client;
   }
 
-  Future<oauth2.Client> getResourceOwnerPasswordGrant(String email,
-      String password) async {
-    bool fileExists = await _credentialFileExists();
-
-    var client = await oauth2.resourceOwnerPasswordGrant(
+  Future<Client> getResourceOwnerPasswordGrant(
+      String email, String password) async {
+    var client = await resourceOwnerPasswordGrant(
         _authorizationEndpoint, email, password,
         identifier: _passwordGrantIdentifier, secret: _passwordGrantSecret);
 
-    if (fileExists)
-      await _credentialsFile.writeAsString(client.credentials.toJson());
-    else
-      throw Exception("Couldn't save credentials. File doesn't exist.");
+    await _getCredentialsFile();
+    await _credentialsFile.writeAsString(client.credentials.toJson());
 
     return client;
   }
 
-  Future<oauth2.Client> loadCredentialsFromFile(BuildContext context) async {
-    Directory directory = await getApplicationDocumentsDirectory();
-    _credentialsFile = File('${directory.path}/credentials.json');
-    bool fileExists = await _credentialsFile.exists();
+  Future<Client> loadCredentialsFromFile(BuildContext context) async {
+    await _getCredentialsFile();
 
-    if (fileExists) {
-      var credentials =
-      oauth2.Credentials.fromJson(await _credentialsFile.readAsString());
-      if (credentials.isExpired && credentials.canRefresh)
-        credentials = await credentials.refresh(identifier: _passwordGrantIdentifier, secret: _passwordGrantSecret);
-      else {
-        DoggoToast.of(context).showToast("Credentials have expired.");
-        Navigator.of(context).popUntil(ModalRoute.withName('/home'));
-      }
-      return oauth2.Client(credentials,
+    var credentials =
+        Credentials.fromJson(await _credentialsFile.readAsString());
+    if (credentials.isExpired && credentials.canRefresh)
+      credentials = await credentials.refresh(
           identifier: _passwordGrantIdentifier, secret: _passwordGrantSecret);
+    else if (credentials.isExpired && !credentials.canRefresh) {
+      DoggoToast.of(context).showToast("Credentials have expired.");
+      Navigator.of(context).popUntil((route) => false);
     }
-    Navigator.of(context).popUntil(ModalRoute.withName('/home'));
-    throw Exception("Credentials file doesn't exist.");
+    return Client(credentials,
+        identifier: _passwordGrantIdentifier, secret: _passwordGrantSecret);
   }
 }
