d6622350ee14f53dabf9e714ee61f71e84576d84
mnajborowski <michal.najborowski@gmail.com>
2020-11-21 16:19:42 +0100

    connect to new maps backend

--- lib/Location/add_location_bottom_sheet_widget.dart
+++ lib/Location/add_location_bottom_sheet_widget.dart
@@ -1,32 +1,29 @@
 import 'dart:convert';
 import 'dart:ui';
 
+import 'package:doggo_frontend/Custom/doggo_toast.dart';
 import 'package:doggo_frontend/Location/http/location.dart';
+import 'package:doggo_frontend/OAuth2/oauth2_client.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:http/http.dart' as http;
+import 'package:oauth2/oauth2.dart';
 import 'package:uuid/uuid.dart';
 
 typedef AddMarkerToMapCallback = void Function(LocationMarker locationMarker);
 
-class AddLocationBottomSheetWidget extends StatefulWidget {
-  final LatLng latLng;
-  final AddMarkerToMapCallback addMarkerToMapCallback;
-
-  const AddLocationBottomSheetWidget(
-      {Key key, @required this.latLng, @required this.addMarkerToMapCallback})
-      : super(key: key);
-
-  @override
-  _AddLocationBottomSheetWidgetState createState() =>
-      _AddLocationBottomSheetWidgetState();
-}
-
 class _AddLocationBottomSheetWidgetState
     extends State<AddLocationBottomSheetWidget> {
+  Client client;
+  final url = 'https://doggo-service.herokuapp.com/api/dog-lover/mapMarkers';
+  final headers = {'Content-Type': 'application/json', 'Accept': '*/*'};
+
   final nameController = TextEditingController();
   final descriptionController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   void dispose() {
@@ -35,23 +32,9 @@ class _AddLocationBottomSheetWidgetState
     super.dispose();
   }
 
-  @override
-  void initState() {
-    super.initState();
-  }
-
   Future _saveMarker(LocationMarker locationMarker) async {
-    final storage = FlutterSecureStorage();
-    final token = await storage.read(key: 'token');
-
-    final url = 'https://doggo-app-server.herokuapp.com/api/mapMarkers';
-    final headers = {
-      'Content-Type': 'application/json',
-      'Accept': '*/*',
-      'Authorization': 'Bearer $token'
-    };
-
-    final requestBody = jsonEncode({
+    client ??= await OAuth2Client().loadCredentialsFromFile(context);
+    final body = jsonEncode({
       'id': locationMarker.id,
       'name': locationMarker.name,
       'description': locationMarker.description,
@@ -59,31 +42,28 @@ class _AddLocationBottomSheetWidgetState
       'longitude': locationMarker.longitude,
     });
 
-    final response = await http.post(url, headers: headers, body: requestBody);
+    final response = await client.post(url, headers: headers, body: body);
     switch (response.statusCode) {
-      case 200:
+      case 201:
         {
           Navigator.of(context).pop();
           return widget.addMarkerToMapCallback(locationMarker);
         }
       case 400:
         {
-          _showAlertDialogWithMessage(
-            "You have to provide a name for new location!",
-          );
+          DoggoToast.of(context).showToast('You have to provide a name for new location.');
           throw Exception("Location marker name cannot be empty!");
         }
       case 409:
         {
-          _showAlertDialogWithMessage(
-            'The place you want to add is too close to already existing one!',
-          );
+          DoggoToast.of(context).showToast('The place you want to add is too close to already existing one.');
           throw Exception(
             'The place to add is too close to already existing one!',
           );
         }
       default:
         {
+          DoggoToast.of(context).showToast("Couldn't save location marker on server!");
           throw Exception("Couldn't save location marker on server!");
         }
     }
@@ -169,15 +149,17 @@ class _AddLocationBottomSheetWidgetState
       ),
     );
   }
+}
 
-  Future _showAlertDialogWithMessage(String message) {
-    return showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          content: Text(message),
-        );
-      },
-    );
-  }
+class AddLocationBottomSheetWidget extends StatefulWidget {
+  final LatLng latLng;
+  final AddMarkerToMapCallback addMarkerToMapCallback;
+
+  const AddLocationBottomSheetWidget(
+      {Key key, @required this.latLng, @required this.addMarkerToMapCallback})
+      : super(key: key);
+
+  @override
+  _AddLocationBottomSheetWidgetState createState() =>
+      _AddLocationBottomSheetWidgetState();
 }
